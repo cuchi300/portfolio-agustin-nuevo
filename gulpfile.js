4eb013efb4a07192479c27f9fd3a347efa893da2
Agustin <abaiud55@gmail.com>
2024-04-22 23:39:36 -0300

    actualizado 22-04

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,18 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const rename = require('gulp-rename');
 const purgecss = require('gulp-purgecss');
+const terser = require('gulp-terser-js');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
 
 const imagemin = require('gulp-imagemin');
 const webp =  require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css() {
     return src('src/scss/app.scss')
         .pipe( sass() )
+        .pipe(postcss([ cssnano() ]))
         .pipe( dest('build/css') )
 }
 
@@ -27,6 +32,20 @@ function imagenWebp() {
         .pipe( webp(opciones))
         .pipe( dest('build/img') )
 }
+function imagenAvif() {
+    const opciones = {
+        quality: 50
+    }
+    return src('src/img/**/*.{jpg,png}')
+        .pipe( avif(opciones))
+        .pipe( dest('build/img') )
+}
+
+function javascript() {
+    return src('src/js/**/*.js')
+        .pipe(terser())
+        .pipe(dest('build/js'))
+}
 
 function cssbuild() {
     return src('build/css/app.css')
@@ -34,7 +53,7 @@ function cssbuild() {
             suffix: '.min'
         }))
         .pipe( purgecss({
-            content: ['index.html', 'js/scroll.js', 'js/app.js' ,'js/efectos.js', 'js/bootstrap.bundle.min.js'],
+            content: ['index.html', 'src/js/scroll.js', 'src/js/app.js' ,'src/js/efectos.js', 'src/js/bootstrap.bundle.min.js'],
         }))
         .pipe( dest('build/css') )
 }
@@ -49,5 +68,7 @@ exports.css = css;
 exports.imagenes = imagenes;
 exports.imagenWebp = imagenWebp;
 exports.dev = dev;
+exports.imagenAvif = imagenAvif;
+exports.javascript = javascript;
 exports.cssbuild = cssbuild;
-exports.default = series(css, dev);
+exports.default = series(imagenes, imagenAvif, imagenWebp);
